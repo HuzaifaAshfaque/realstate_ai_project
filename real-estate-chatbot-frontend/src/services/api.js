bb1019776cae8f194fa74c522f975bccd571c6e2
Huzaifa Ashfaque <siddiqui8573@gmail.com>
2025-05-16 14:18:56 +0530

    Update APi.js

--- real-estate-chatbot-frontend/src/services/api.js
+++ real-estate-chatbot-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
-
+console.log("API_URL",API_URL);
 const ApiService = {
   // Send query to backend
   sendQuery: async (query, file = null) => {
